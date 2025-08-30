4dc94e7435ff3aaa6bfb38ceff5d083dbd15223b
marlonsvl <marlon_santo@hotmail.com>
2025-08-29 21:48:57 -0500

    Fix: add Django to requirements

--- sentiment_eval/settings.py
+++ sentiment_eval/settings.py
@@ -110,11 +110,7 @@ WSGI_APPLICATION = 'sentiment_eval.wsgi.application'
 
 import dj_database_url
 DATABASES = {
-    "default": dj_database_url.config(
-        default="postgresql://postgres:postgres@localhost:5432/postgres",
-        conn_max_age=600,
-        ssl_require=True
-    )
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
